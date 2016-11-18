adfa008ee2d4c8a6bb911128b6d541ec89bea406
Akinyele Akintola-Febrissy <aakin013@uottawa.ca>
2016-11-18 17:43:32 -0500

    Added the current "Backend" files :tada:

---
+++ Backend/lib/db_adapter.rb
@@ -0,0 +1,18 @@
+class DBAdapter
+
+	# This class only contains class methods that will interact with the
+	# database through the provided model's methods.
+
+	def save_model(model, params, required)
+
+	end
+
+	def update_model(model, params, required)
+
+	end
+
+	def delete_model(id)
+
+	end
+
+end
---
+++ Backend/lib/json_parser.rb
@@ -0,0 +1,17 @@
+class JSONParser
+
+	# This is a class method that simply takes a object then transforms
+	# it into a regular json.
+
+	def self.json(code=0, message)
+		json = {
+			code: code,
+			message: message
+		}
+	end
+
+	def self.default
+		self.json(0, "**No message**")
+	end
+
+end
